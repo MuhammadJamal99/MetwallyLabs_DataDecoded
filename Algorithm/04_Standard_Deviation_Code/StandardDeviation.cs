c1eb4a751a904ffee6c7565cf7c717ffdcfb974b
MuhammadJamal99 <muhammadjammi18@gmail.com>
2024-07-20 11:35:07 +0300

    01_Circle_Area_Algorithm_Model_Code

---
+++ Algorithm/04_Standard_Deviation_Code/StandardDeviation.cs
@@ -0,0 +1,33 @@
+﻿namespace Algorithm._02_Sigma_Notation_Standard_Deviation_Algorithm.Sigma_Notation;
+public static class StandardDeviation
+{
+    public static double CalculateStandardDeviation()
+    {
+        double sd = 0, ave = 0, sum = 0;
+
+        Console.WriteLine("Please Enter Size of Array You Want To Calculate Standard Deviation for it.");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int[] numbers = new int[n];
+        int i = 0;
+        while (i < n)
+        {
+            Console.Write($"Enter number {i + 1}: ");
+            numbers[i] = Convert.ToInt32(Console.ReadLine());
+            ave += numbers[i];
+            i++;
+        }
+
+        i = 0;
+        ave /= n;
+
+        while (i < numbers.Length)
+        {
+            var num = numbers[i];
+            sum += Math.Pow((numbers[i] - ave), 2);
+            i++;
+        }
+        sum /= n;
+        sd = Math.Sqrt(sum);
+        return sd;
+    }
+}
--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -1,4 +1,12 @@
-﻿using Algorithm.Circle;
+﻿using Algorithm._02_Sigma_Notation_Standard_Deviation_Algorithm.Sigma_Notation;
 
-Console.WriteLine(Circle.CalculateCircleArea(10));
-Console.WriteLine(Circle.CalculateCircleArea(20));
+Console.WriteLine(StandardDeviation.CalculateStandardDeviation());
+
+
+
+
+
+
+//Console.WriteLine(Circle.CalculateCircleArea(20));
+//Console.WriteLine(SigmaNotation.CalculateSigmaNotation(3));
+//Console.WriteLine(Circle.CalculateCircleArea(10));
